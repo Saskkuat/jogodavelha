da66d4d1a12eb4853079764362c821fac7faae26
Saskkuat <saskkuat.dev@gmail.com>
2024-09-28 14:13:39 -0300

    fix css

---
+++ docs/js/common.js
@@ -0,0 +1,22 @@
+window.ShowSwal = (player) => {
+    Swal.fire({
+        title: player + ' won!!',
+        width: 350,
+        padding: '3em',
+        color: '#716add',
+        backdrop: `
+                        rgba(0,0,123,0.4)
+                      `
+    })
+}
+window.ShowTie = () => {
+    Swal.fire({
+        title: 'Go home, nobody won!',
+        width: 350,
+        padding: '3em',
+        color: '#716add',
+        backdrop: `
+                        rgba(0,0,123,0.4)
+                      `
+    })
+}
